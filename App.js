32f1bd54d2bb64e212381ec15c0aeb84ef1c18f3
Andrii Veremii <dashuk10@gmail.com>
2023-08-16 23:47:59 +0200

    add

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import "react-native-gesture-handler";
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+// import { useSelector } from "react-redux";
 import { useFonts } from "expo-font";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { store, persistor } from "./redux/store";
-import { selectLoginState } from "./redux/auth/selectors";
+// import { selectLoginState } from "./redux/auth/selectors";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import { auth } from './config';
+import { updateProfile } from "firebase/auth";
 
-import Registration from './Screens/RegistrationScreen';
-import Login from './Screens/LoginScreen';
+import Registration from "./Screens/RegistrationScreen";
+import Login from "./Screens/LoginScreen";
 import Home from "./Screens/Home";
 import Profile from "./Screens/ProfileScreen";
 import Comments from "./Screens/CommentsScreen";
@@ -20,17 +23,54 @@ import Posts from "./Screens/PostsScreen";
 const MainStack = createStackNavigator();
 
 const AppNavigation = () => {
-  const isLoggedIn = useSelector(selectLoginState);
+  const dispatch = useDispatch();
+  const [isLogged, setIsLogged] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, async (user) => {
+      console.log(user);
+      if (user) {
+        const uid = user.uid;
+        const data = {
+          userId: user.uid,
+          login: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+        };
+        // const data = await getUserData(uid);
+        // console.log(date)
+        dispatch(updateProfile({ ...data, uid }));
+
+        // navigation.navigate("Home");
+        console.log("головна");
+        setIsLogged(true);
+      } else {
+        console.log("логін");
+        // navigation.navigate("Login");
+        setIsLogged(false);
+      }
+    });
+  }, []); // {isLogged ? "Home" : "Login"}
+
+  //--------------------------------------------
+
+  // const [initialRouteName, setInitialRouteName] = useState(null);
+
+  // useEffect(() => {
+  //   auth.onAuthStateChanged((user) =>
+  //     setInitialRouteName(user ? "Home" : "Login")
+  //   );
+  // }, []);
+
+  // console.log("стан -->", initialRouteName)
 
   return (
     <NavigationContainer>
-      <MainStack.Navigator initialRouteName={isLoggedIn ? "Home" : "Login"}>
+      <MainStack.Navigator initialRouteName={isLogged ? "Home" : "Login"}>
         <MainStack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-        <MainStack.Screen
-          name="Registration"
-          component={Registration}
-          options={{ headerShown: false }}
-        />
+        <MainStack.Screen name="Registration" component={Registration} options={{ headerShown: false }} />
         <MainStack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <MainStack.Screen name="Posts" component={Posts} />
         <MainStack.Screen name="Comments" component={Comments} />
@@ -42,7 +82,6 @@ const AppNavigation = () => {
 };
 
 export default function App() {
-
   const [fontsLoaded] = useFonts({
     "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
     "roboto-medium": require("./assets/fonts/Roboto-Medium.ttf"),
@@ -56,7 +95,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <AppNavigation/>
+        <AppNavigation />
       </PersistGate>
     </Provider>
   );
